d1a165e352050b4320a0dcea632f59443ad32831
nataliafleitas <nataliafleitas2298@gmail.com>
2020-02-06 16:44:27 -0300

    Agrege eslint

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Galeria from '../src/component/galeria1/galeria';
+import Galeria from './component/galeria1/galeria';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
         <p>
-        <h1> Nati</h1>
+          <h1> Nati</h1>
         </p>
-        <FormApp/>
-        <Galeria/>
-        
+        <FormApp />
+        <Galeria />
+
       </header>
     </div>
   );
 }
 
-class FormApp extends React.Component{
-
-  render(){
-    return(
+class FormApp extends React.Component {
+  render() {
+    return (
       <div className="formulario">
         <label htmlFor="username"> Nombre de usuario</label>
         <input name="username" id="username" type="text" />
@@ -31,14 +29,12 @@ class FormApp extends React.Component{
         <label htmlFor="useLocalidad">Localidad</label>
         <input name="username" id="username" type="text" />
         <label htmlFor="useEmail"> Email</label>
-        <input type="submit" value="enviar"/>
-       
+        <input type="submit" value="enviar" />
+
       </div>
-    )
+    );
   }
 }
-
-
 
 
 export default App;
