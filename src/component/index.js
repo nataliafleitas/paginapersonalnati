18f1a2d37e1e1258ddafe64816f0bcd64aa03a16
nataliafleitas <nataliafleitas2298@gmail.com>
2020-02-21 17:00:08 -0300

    Se pudo extender la nieve y mejorar codigo

--- src/component/index.js
+++ src/component/index.js
@@ -4,212 +4,38 @@ import style from './style.module.scss';
 
 
 class Fondo extends React.Component{
+   
+  constructor(props){
+    super(props);
+    this.contenedorNieve = React.createRef(); /* ES como un querySelector, pero con react, o sea que hace referencia elementos*/
+  }
 
-    render(){
+  componentDidMount(){/*  cuando se termina de dibujar el componente, llama a esta funcion, osea lo que esta adentro*/
+   this.dibujarSpans()
+  }
+
+   dibujarSpans = () => {
+     let numSpans = 400;
+     let spans;
+     for(let i = 0 ; i < numSpans ; i++){
+       spans += `<div> </div>`;
+     };
+
+     this.contenedorNieve.current.innerHTML = spans;
+
+     let divs = this.contenedorNieve.current.children;
+     
+     for(let i = 0 ; i < divs.length; i++){
+       divs[i].className += style.snow;
+     }
+
+    }
+   render(){
       return(
         <div className={style.container}>
-          <section className={style.fixed}>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>
-          <div className={style.snow}></div>       
-      </section>
+        <section ref={this.contenedorNieve} className={style.fixed}>
+
+         </section>
         {this.props.children}
         </div>
     
